16a7d31e9e683e7ca0ab0454da47f7df643c155e
= <mbfrahry@gmail.com>
2017-02-22 15:33:15 -0700

    Split defer functions

--- compute/security_associations_test.go
+++ compute/security_associations_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/hashicorp/go-oracle-terraform/opc"
 )
 
+var createdSecurityAssociation *SecurityAssociationInfo
+
 func TestAccSecurityAssociationLifeCycle(t *testing.T) {
 	helper.Test(t, helper.TestCase{})
 
@@ -16,16 +18,26 @@ func TestAccSecurityAssociationLifeCycle(t *testing.T) {
 	)
 
 	defer func() {
-		if err := tearDownInstances(); err != nil {
-			log.Printf("Error deleting instance: %#v", createdInstance)
-			log.Print("Dangling resources may occur!")
-			t.Fatalf("Error: %v", err)
-		}
 		if err := tearDownSecurityLists(); err != nil {
 			log.Printf("Error deleting security list: %#v", createdSecurityList)
 			log.Print("Dangling resources may occur!")
 			t.Fatalf("Error: %v", err)
 		}
+	}()
+	defer func() {
+		if err := tearDownInstances(); err != nil {
+			log.Printf("Error deleting instance: %#v", createdInstance)
+			log.Print("Dangling resources may occur!")
+			t.Fatalf("Error: %v", err)
+		}
+	}()
+	defer func() {
+		if err := tearDownSecurityAssociations(); err != nil {
+			log.Printf("Error deleting security association: %#v", createdSecurityAssociation)
+			log.Print("Dangling resources may occur!")
+			t.Fatalf("Error: %v", err)
+		}
+		log.Printf("Successfully deleted Security Association")
 	}()
 
 	svc, err := getInstancesClient()
@@ -77,32 +89,23 @@ func TestAccSecurityAssociationLifeCycle(t *testing.T) {
 		SecList: createdSecurityList.Name,
 		VCable:  createdInstance.VCableID,
 	}
-	securityAssociation, err := securityAssociationClient.CreateSecurityAssociation(&createSecurityAssociationInput)
+	createdSecurityAssociation, err = securityAssociationClient.CreateSecurityAssociation(&createSecurityAssociationInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Printf("Successfully created Security Association: %+v", securityAssociation)
+	log.Printf("Successfully created Security Association: %+v", createdSecurityAssociation)
 
 	getSecurityAssociationInput := GetSecurityAssociationInput{
-		Name: securityAssociation.Name,
+		Name: createdSecurityAssociation.Name,
 	}
 	getSecurityAssociationOutput, err := securityAssociationClient.GetSecurityAssociation(&getSecurityAssociationInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if securityAssociation.VCable != getSecurityAssociationOutput.VCable {
-		t.Fatalf("Created and retrived vcables don't match.\n Desired: %s\n Actual: %s", securityAssociation.VCable, getSecurityAssociationOutput.VCable)
+	if createdSecurityAssociation.VCable != getSecurityAssociationOutput.VCable {
+		t.Fatalf("Created and retrived vcables don't match.\n Desired: %s\n Actual: %s", createdSecurityAssociation.VCable, getSecurityAssociationOutput.VCable)
 	}
 	log.Printf("Successfully retrieved Security Association")
-
-	deleteSecurityAssociationInput := DeleteSecurityAssociationInput{
-		Name: securityAssociation.Name,
-	}
-	err = securityAssociationClient.DeleteSecurityAssociation(&deleteSecurityAssociationInput)
-	if err != nil {
-		t.Fatal(err)
-	}
-	log.Printf("Successfully deleted Security Association")
 }
 
 func getSecurityAssociationsClient() (*SecurityAssociationsClient, error) {
@@ -112,4 +115,17 @@ func getSecurityAssociationsClient() (*SecurityAssociationsClient, error) {
 	}
 
 	return client.SecurityAssociations(), nil
+}
+
+func tearDownSecurityAssociations() error {
+	sac, err := getSecurityAssociationsClient()
+	if err != nil {
+		return err
+	}
+
+	input := &DeleteSecurityAssociationInput{
+		Name: createdSecurityAssociation.Name,
+	}
+
+	return sac.DeleteSecurityAssociation(input)
 }
