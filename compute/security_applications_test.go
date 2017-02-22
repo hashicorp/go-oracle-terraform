af576a03a74199b613e4985ee259e67886edc2d4
tombuildsstuff <git@ibuildstuff.co.uk>
2017-02-22 08:45:17 -0800

    Acceptance tests for Create/Read/Delete

---
+++ compute/security_applications_test.go
@@ -0,0 +1,100 @@
+package compute
+
+import (
+	"github.com/hashicorp/go-oracle-terraform/helper"
+	"github.com/hashicorp/go-oracle-terraform/opc"
+	"log"
+	"reflect"
+	"testing"
+)
+
+func TestAccSecurityApplicationsTCPLifeCycle(t *testing.T) {
+	helper.Test(t, helper.TestCase{})
+
+	securityApplicationsClient, err := getSecurityApplicationsClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("Obtained Security Applications List Client")
+
+	name := "test-sec-app-tcp"
+	protocol := "all"
+	dport := "19336"
+	icmpType := ""
+	icmpCode := ""
+	description := "Terraform Acceptance Test TCP Lifecycle"
+
+	createdSecurityApplication, err := securityApplicationsClient.CreateSecurityApplication(name, protocol, dport, icmpType, icmpCode, description)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("Successfully created Security Application: %+v", createdSecurityApplication)
+
+	retrievedSecurityApplication, err := securityApplicationsClient.GetSecurityApplication(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(createdSecurityApplication, retrievedSecurityApplication) {
+		t.Fatalf("Retrieved Security Application did not match Expected. \nDesired: %s \nActual: %s", createdSecurityApplication, retrievedSecurityApplication)
+	}
+
+	log.Printf("Successfully retrieved Security Application")
+
+	err = securityApplicationsClient.DeleteSecurityApplication(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("Successfully deleted Security Application")
+}
+
+func TestAccSecurityApplicationsICMPLifeCycle(t *testing.T) {
+	helper.Test(t, helper.TestCase{})
+
+	securityApplicationsClient, err := getSecurityApplicationsClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("Obtained Security Applications List Client")
+
+	name := "test-sec-app-icmp"
+	protocol := "icmp"
+	dport := ""
+	icmpType := "echo"
+	icmpCode := ""
+	description := "Terraform Acceptance Test ICMP Lifecycle"
+
+	createdSecurityApplication, err := securityApplicationsClient.CreateSecurityApplication(name, protocol, dport, icmpType, icmpCode, description)
+	if err != nil {
+		t.Fatal(err)
+	}
+	log.Printf("Successfully created Security Application: %+v", createdSecurityApplication)
+
+	retrievedSecurityApplication, err := securityApplicationsClient.GetSecurityApplication(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(createdSecurityApplication, retrievedSecurityApplication) {
+		t.Fatalf("Retrieved Security Application did not match Expected. \nDesired: %s \nActual: %s", createdSecurityApplication, retrievedSecurityApplication)
+	}
+
+	log.Printf("Successfully retrieved Security Application")
+
+	err = securityApplicationsClient.DeleteSecurityApplication(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("Successfully deleted Security Application")
+}
+
+func getSecurityApplicationsClient() (*SecurityApplicationsClient, error) {
+	client, err := getTestClient(&opc.Config{})
+	if err != nil {
+		return &SecurityApplicationsClient{}, err
+	}
+
+	return client.SecurityApplications(), nil
+}
