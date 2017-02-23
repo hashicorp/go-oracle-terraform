90180be449bc5aa7327afe05231cfe9da15d5c50
tombuildsstuff <git@ibuildstuff.co.uk>
2017-02-22 17:11:52 -0800

    Removing the CRU unit tests since they're covered by the AT's

--- compute/storage_volumes_test.go
+++ compute/storage_volumes_test.go
@@ -5,89 +5,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"reflect"
 	"testing"
 
 	"github.com/hashicorp/go-oracle-terraform/helper"
 )
-
-// Test that the client can create a storage volume.
-func TestAccStorageVolumeClient_CreateStorageVolume(t *testing.T) {
-	helper.Test(t, helper.TestCase{})
-	server := newAuthenticatingServer(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			t.Fatalf("Wrong HTTP method %s, expected POST", r.Method)
-		}
-
-		expectedPath := "/storage/volume/"
-		if r.URL.Path != expectedPath {
-			t.Fatalf("Wrong HTTP URL %v, expected %v", r.URL, expectedPath)
-		}
-
-		spec := &CreateStorageVolumeInput{}
-		unmarshalRequestBody(t, r, spec)
-
-		if spec.Size != "15G" {
-			t.Fatalf("Expected spec size of 15G, was %s", spec.Size)
-		}
-		w.WriteHeader(201)
-	})
-
-	defer server.Close()
-	sv, err := getStubStorageVolumeClient(server)
-	if err != nil {
-		t.Fatalf("error getting stub client: %s", err)
-	}
-
-	volume := CreateStorageVolumeInput{
-		Name: "myVolume",
-		Size: "15G",
-	}
-	err = sv.CreateStorageVolume(&volume)
-	if err != nil {
-		t.Fatalf("Create storage volume request failed: %s", err)
-	}
-}
-
-func TestAccStorageVolumeClient_GetStorageVolume(t *testing.T) {
-	helper.Test(t, helper.TestCase{})
-	server := newAuthenticatingServer(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			t.Fatalf("Expected GET request, was %s", r.Method)
-		}
-
-		if r.URL.String() != "/storage/volume/Compute-test/test/myVolume/" {
-			t.Fatalf("Expected request to /storage/volume/Compute-test/test/myVolume/, path was %s", r.URL.String())
-		}
-
-		svr := "{\"result\":[{\"name\":\"/Compute-test/test/test\",\"size\":\"16G\",\"status\":\"Online\"}]}"
-
-		w.Write([]byte(svr))
-		w.WriteHeader(200)
-	})
-
-	defer server.Close()
-	sv, err := getStubStorageVolumeClient(server)
-	if err != nil {
-		t.Fatalf("error getting stub client: %s", err)
-	}
-
-	getRequest := &GetStorageVolumeInput{
-		Name: "myVolume",
-	}
-	info, err := sv.GetStorageVolume(getRequest)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(info.Result) == 0 {
-		t.Fatal("Expected StorageVolumeInfo in result, but was empty.")
-	}
-
-	if info.Result[0].Size != "16G" {
-		t.Fatalf("Expected info with size of 16G, was %s", info.Result[0].Size)
-	}
-}
 
 func TestAccStorageVolumeClient_WaitForStorageVolumeOnline(t *testing.T) {
 	helper.Test(t, helper.TestCase{})
@@ -126,69 +47,6 @@ func TestAccStorageVolumeClient_WaitForStorageVolumeOnlineTimeout(t *testing.T) 
 	}
 }
 
-func TestAccStorageVolumeClient_UpdateStorageVolume(t *testing.T) {
-	helper.Test(t, helper.TestCase{})
-	server := newAuthenticatingServer(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			svr := "{\"result\":[{\"name\":\"/Compute-test/test/test\",\"size\":\"16G\",\"status\":\"Online\"}]}"
-
-			w.Write([]byte(svr))
-			w.WriteHeader(200)
-			return
-		}
-
-		if r.URL.String() != "/storage/volume/Compute-test/test/myVolume/" {
-			t.Errorf("Expected request to foo, path was %s", r.URL.String())
-		}
-
-		info := &StorageVolumeInfo{}
-		unmarshalRequestBody(t, r, info)
-
-		if info.Size != "12G" {
-			t.Errorf("Expected updated storage to be 12G, was %s", info.Size)
-		}
-
-		if info.Description != "updated description" {
-			t.Errorf("Expected description to be 'updated description', was %s", info.Description)
-		}
-
-		if !reflect.DeepEqual(info.Tags, []string{"foo", "bar"}) {
-			t.Errorf("Expected updated tags to be [foo, bar], was %s", info.Tags)
-		}
-		w.WriteHeader(200)
-	})
-
-	defer server.Close()
-	sv, err := getStubStorageVolumeClient(server)
-	if err != nil {
-		t.Fatalf("error getting stub client: %s", err)
-	}
-
-	updateRequest := &UpdateStorageVolumeInput{
-		Name:        "myVolume",
-		Size:        "12G",
-		Description: "updated description",
-		Tags:        []string{"foo", "bar"},
-	}
-	err = sv.UpdateStorageVolume(updateRequest)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func getStubStorageVolumeClient(server *httptest.Server) (*StorageVolumeClient, error) {
-	endpoint, err := url.Parse(server.URL)
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := getStubClient(endpoint)
-	if err != nil {
-		return nil, err
-	}
-	return client.StorageVolumes(), nil
-}
-
 func serverThatReturnsOnlineStorageVolumeAfterThreeSeconds(t *testing.T) *httptest.Server {
 	count := 0
 	return newAuthenticatingServer(func(w http.ResponseWriter, r *http.Request) {
@@ -205,4 +63,17 @@ func serverThatReturnsOnlineStorageVolumeAfterThreeSeconds(t *testing.T) *httpte
 		w.Write([]byte(svr))
 		w.WriteHeader(200)
 	})
+}
+
+func getStubStorageVolumeClient(server *httptest.Server) (*StorageVolumeClient, error) {
+	endpoint, err := url.Parse(server.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := getStubClient(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	return client.StorageVolumes(), nil
 }
