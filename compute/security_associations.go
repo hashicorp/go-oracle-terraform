32b21a7e447dbcf2959942801a68dfd4db28ba1b
tombuildsstuff <git@ibuildstuff.co.uk>
2017-03-02 11:22:40 +0000

    Documentation for Security Associations

--- compute/security_associations.go
+++ compute/security_associations.go
@@ -19,16 +19,28 @@ func (c *Client) SecurityAssociations() *SecurityAssociationsClient {
 
 // SecurityAssociationInfo describes an existing security association.
 type SecurityAssociationInfo struct {
+	// The three-part name of the Security Association (/Compute-identity_domain/user/object).
 	Name    string `json:"name"`
+	// The name of the Security List that you want to associate with the instance.
 	SecList string `json:"seclist"`
+	// vCable of the instance that you want to associate with the security list.
 	VCable  string `json:"vcable"`
+	// Uniform Resource Identifier
 	URI     string `json:"uri"`
 }
 
 // CreateSecurityAssociationInput defines a security association to be created.
 type CreateSecurityAssociationInput struct {
+	// The three-part name of the Security Association (/Compute-identity_domain/user/object).
+	// If you don't specify a name for this object, then the name is generated automatically.
+	// Object names can contain only alphanumeric characters, hyphens, underscores, and periods. Object names are case-sensitive.
+	// Required
 	Name    string `json:"name"`
+	// The name of the Security list that you want to associate with the instance.
+	// Required
 	SecList string `json:"seclist"`
+	// The name of the vCable of the instance that you want to associate with the security list.
+	// Required
 	VCable  string `json:"vcable"`
 }
 
@@ -47,6 +59,8 @@ func (c *SecurityAssociationsClient) CreateSecurityAssociation(createInput *Crea
 
 // GetSecurityAssociationInput describes the security association to get
 type GetSecurityAssociationInput struct {
+	// The three-part name of the Security Association (/Compute-identity_domain/user/object).
+	// Required
 	Name string `json:"name"`
 }
 
@@ -62,6 +76,8 @@ func (c *SecurityAssociationsClient) GetSecurityAssociation(getInput *GetSecurit
 
 // DeleteSecurityAssociationInput describes the security association to delete
 type DeleteSecurityAssociationInput struct {
+	// The three-part name of the Security Association (/Compute-identity_domain/user/object).
+	// Required
 	Name string `json:"name"`
 }
 
