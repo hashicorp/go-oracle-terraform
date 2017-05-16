546ddb7c8fbf97f0d51bf8d04622693ced32e642
= <mbfrahry@gmail.com>
2017-05-15 19:41:14 -0600

    Finished up to testing

--- compute/snapshots.go
+++ compute/snapshots.go
@@ -17,65 +17,71 @@ func (c *Client) Snapshots() *SnapshotsClient {
 		}}
 }
 
+type SnapshotDelay string
+
+const (
+	SnapshotDelayShutdown SnapshotDelay = "shutdown"
+)
+
 // SnapshotInfo describes an existing Snapshot.
 type Snapshot struct {
-  // Shows the default account for your identity domain.
-  Account string `json:"account"`
-  // Timestamp when this request was created.
-  CreationTime string `json:"creation_time"`
-  // Snapshot of the instance is not taken immediately.
-  Delay string `json:"delay"`
-  // A description of the reason this request entered "error" state.
-  ErrorReason string `json:"error_reason"`
-  // Name of the instance
-  Instance string `json:"instance"`
-  // Name of the machine image generated from the instance snapshot request.
-  MachineImage string `json:"machineimage"`
-  // Name of the instance snapshot request.
-  Name string `json:"name"`
-  // Not used
-  Quota string `json:"quota"`
-  // The state of the request.
-  State string `json:"state"`
-  // Uniform Resource Identifier
-  URI string `json:"uri"`
+	// Shows the default account for your identity domain.
+	Account string `json:"account"`
+	// Timestamp when this request was created.
+	CreationTime string `json:"creation_time"`
+	// Snapshot of the instance is not taken immediately.
+	Delay SnapshotDelay `json:"delay"`
+	// A description of the reason this request entered "error" state.
+	ErrorReason string `json:"error_reason"`
+	// Name of the instance
+	Instance string `json:"instance"`
+	// Name of the machine image generated from the instance snapshot request.
+	MachineImage string `json:"machineimage"`
+	// Name of the instance snapshot request.
+	Name string `json:"name"`
+	// Not used
+	Quota string `json:"quota"`
+	// The state of the request.
+	State string `json:"state"`
+	// Uniform Resource Identifier
+	URI string `json:"uri"`
 }
 
 // CreateSnapshotInput defines an Snapshot to be created.
 type CreateSnapshotInput struct {
-  //The name of the account that contains the credentials and access details of
-  // Oracle Storage Cloud Service. The machine image file is uploaded to the Oracle
-  // Storage Cloud Service account that you specify.
-  // Optional
-  Account string `json:"account"`
-  // Use this option when you want to take preserve the custom changes you have made
-  // to an instance before deleting the instance. The only permitted value is shutdown.
-  // Snapshot of the instance is not taken immediately. It creates a machine image which
-  // preserves the changes you have made to the instance, and then the instance is deleted.
-  // Note: This option has no effect if you shutdown the instance from inside it. Any pending
-  // snapshot request on that instance goes into error state. You must delete the instance
-  // (DELETE /instance/{name}).
-  // Optional
-  Delay string `json:"delay"`
-  // Name of the instance that you want to clone.
-  // Required
-  Instance string `json:"instance"`
-  // Specify the name of the machine image created by the snapshot request.
-  // Object names can contain only alphanumeric characters, hyphens, underscores, and periods.
-  // Object names are case-sensitive.
-  // If you don't specify a name for this object, then the name is generated automatically.
-  // Optional
-  MachineImage string `json:"machineimage"`
+	//The name of the account that contains the credentials and access details of
+	// Oracle Storage Cloud Service. The machine image file is uploaded to the Oracle
+	// Storage Cloud Service account that you specify.
+	// Optional
+	Account string `json:"account"`
+	// Use this option when you want to preserve the custom changes you have made
+	// to an instance before deleting the instance. The only permitted value is shutdown.
+	// Snapshot of the instance is not taken immediately. It creates a machine image which
+	// preserves the changes you have made to the instance, and then the instance is deleted.
+	// Note: This option has no effect if you shutdown the instance from inside it. Any pending
+	// snapshot request on that instance goes into error state. You must delete the instance
+	// (DELETE /instance/{name}).
+	// Optional
+	Delay string `json:"delay"`
+	// Name of the instance that you want to clone.
+	// Required
+	Instance string `json:"instance"`
+	// Specify the name of the machine image created by the snapshot request.
+	// Object names can contain only alphanumeric characters, hyphens, underscores, and periods.
+	// Object names are case-sensitive.
+	// If you don't specify a name for this object, then the name is generated automatically.
+	// Optional
+	MachineImage string `json:"machineimage"`
 }
 
 // CreateSnapshot creates a new Snapshot
 func (c *SnapshotsClient) CreateSnapshot(createInput *CreateSnapshotInput) (*Snapshot, error) {
-  createInput.Account = c.getQualifiedACMEName(createInput.Account)
+	createInput.Account = c.getQualifiedACMEName(createInput.Account)
 	createInput.Instance = c.getQualifiedName(createInput.Instance)
 	createInput.MachineImage = c.getQualifiedName(createInput.MachineImage)
 
 	var snapshotInfo Snapshot
-	if err := c.createResource(&createInput, &snapShot); err != nil {
+	if err := c.createResource(&createInput, &snapshotInfo); err != nil {
 		return nil, err
 	}
 
@@ -84,13 +90,13 @@ func (c *SnapshotsClient) CreateSnapshot(createInput *CreateSnapshotInput) (*Sna
 
 // GetSnapshotInput describes the snapshot to get
 type GetSnapshotInput struct {
-	// The name of the Snapshot (/Compute-identity_domain/user/object).
+	// The name of the Snapshot
 	Name string `json:name`
 }
 
 // GetSnapshot retrieves the Snapshot with the given name.
 func (c *SnapshotsClient) GetSnapshot(getInput *GetSnapshotInput) (*Snapshot, error) {
-	getInput.
+	getInput.Name = c.getQualifiedName(getInput.Name)
 	var snapshotInfo Snapshot
 	if err := c.getResource(getInput.Name, &snapshotInfo); err != nil {
 		return nil, err
@@ -101,7 +107,8 @@ func (c *SnapshotsClient) GetSnapshot(getInput *GetSnapshotInput) (*Snapshot, er
 
 // DeleteSnapshotInput describes the snapshot to delete
 type DeleteSnapshotInput struct {
-
+	// The name of the Snapshot
+	Name string `json:name`
 }
 
 // DeleteSnapshot deletes the Snapshot with the given name.
@@ -114,5 +121,5 @@ func (c *SnapshotsClient) success(snapshotInfo *Snapshot) (*Snapshot, error) {
 	c.unqualify(&snapshotInfo.Instance)
 	c.unqualify(&snapshotInfo.MachineImage)
 	c.unqualify(&snapshotInfo.Name)
-	return keyInfo, nil
+	return snapshotInfo, nil
 }
--- compute/snapshots_test.go
+++ compute/snapshots_test.go
@@ -2,79 +2,93 @@ package compute
 
 import (
 	"log"
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/go-oracle-terraform/helper"
 	"github.com/hashicorp/go-oracle-terraform/opc"
 )
 
-func TestAccSSHKeyLifeCycle(t *testing.T) {
+const (
+	_SnapshotInstanceTestName  = "test-acc-snapshot"
+	_SnapshotInstanceTestLabel = "test"
+	_SnapshotInstanceTestShape = "oc3"
+	_SnapshotInstanceTestImage = "/oracle/public/Oracle_Solaris_11.3"
+)
+
+func TestAccSnapshotLifeCycle(t *testing.T) {
 	helper.Test(t, helper.TestCase{})
 
-	name := "test-key"
-
-	sshKeyClient, err := getSSHKeysClient()
+	sClient, iClient, err := getSnapshotsTestClients()
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Printf("Obtained SSH Key Client\n")
 
-	createSSHKeyInput := CreateSSHKeyInput{
-		Name:    name,
-		Key:     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7BzZyp8CWN7tfIZiZwWx8H9RO2ClKu0ru/+bGEsUmHmSS7aA+iPBVqeK1Kr2nVkoG/32GaMLfVNRlRgZZGTBTFM5nnytNoo3DC9dnIPuIu95TbF1afGkVFNNyeJkC5bQDDaRDMaYBbPVJTa6bA8v7nmzvElQHPRtdRqZnFx80QHdrgTluqhtrxWDBCYMSm2meL/NU11kijoKfYSReT4lroglSxnkvP0vjUqUSvZ6tI231Ggvxg4TU1TL4OgtNyfQgXK585V05n7IT9iiJHThah2/ZGsb0DZimj/D5LxngciXVOkOR1sDt8pQb7QCxgoxOO3sa1K3pFi5UAJQ10tSyhu0yn0AnRG13NWK6DlLKhLzZM5jhGJeeYYuwCL5fzJojflouHgebOO62gqNANkUcf7cWUBJRWjSAYuXe/C6rJOriZuUkC87QpffpYd2WaJmqnjAaj7NaqOTzk5ltpS39EjMenyXWWw1MPs7eEB/A/Rfol0cHzGqoXaIZAJVaEpW7ePWEj193CqSc6uh1nwAT15rvh63z2l1iPL0CbuF4GwZWsIZ6roirmwPpKY79kAls69EKsa7bydSQuYpbU5otkT20FIbtHmyFMYpJzYM6sQHoljO2AHWmWChkYtqglbFPrQgwIrsAHbJtmzNcmbXLUm1AY+SjZd1UYqPBjFDb7w== your_email@example.com",
-		Enabled: true,
+	// In order to get details on a Snapshot we need to create the following resources
+	// - Instance
+
+	instanceInput := &CreateInstanceInput{
+		Name:      _SnapshotInstanceTestName,
+		Label:     _SnapshotInstanceTestLabel,
+		Shape:     _SnapshotInstanceTestShape,
+		ImageList: _SnapshotInstanceTestImage,
+		Storage:   nil,
+		BootOrder: nil,
+		SSHKeys:   []string{},
+		Attributes: map[string]interface{}{
+			"attr1": 12,
+			"attr2": map[string]interface{}{
+				"inner_attr1": "foo",
+			},
+		},
 	}
-	sshKey, err := sshKeyClient.CreateSSHKey(&createSSHKeyInput)
+
+	createdInstance, err := iClient.CreateInstance(instanceInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Printf("Successfully created SSH Key: %+v\n", sshKey)
+	defer tearDownInstances(t, iClient, createdInstance.Name, createdInstance.ID)
 
-	defer tearDownSSHKey(t, sshKeyClient, name)
-
-	getSSHKeyInput := GetSSHKeyInput{
-		Name: name,
+	createSnapshotInput := &CreateSnapshotInput{
+		Instance:     _SnapshotInstanceTestName,
+		MachineImage: _SnapshotInstanceTestName,
 	}
-	getSSHKeyOutput, err := sshKeyClient.GetSSHKey(&getSSHKeyInput)
+	createdSnapshot, err := sClient.CreateSnapshot(createSnapshotInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if createSSHKeyInput.Key != getSSHKeyOutput.Key {
-		t.Fatalf("Created and retrived keys don't match %s\n%s\n", createSSHKeyInput.Key, getSSHKeyOutput.Key)
-	}
-	log.Printf("Successfully retrieved ssh key\n")
+	defer tearDownSnapshots(t, sClient, createdSnapshot.Name)
 
-	updateSSHKeyInput := UpdateSSHKeyInput{
-		Name:    name,
-		Key:     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7BzZyp8CWN7tfIZiZwWx8H9RO2ClKu0ru/+bGEsUmHmSS7aA+iPBVqeK1Kr2nVkoG/32GaMLfVNRlRgZZGTBTFM5nnytNoo3DC9dnIPuIu95TbF1afGkVFNNyeJkC5bQDDaRDMaYBbPVJTa6bA8v7nmzvElQHPRtdRqZnFx80QHdrgTluqhtrxWDBCYMSm2meL/NU11kijoKfYSReT4lroglSxnkvP0vjUqUSvZ6tI231Ggvxg4TU1TL4OgtNyfQgXK585V05n7IT9iiJHThah2/ZGsb0DZimj/D5LxngciXVOkOR1sDt8pQb7QCxgoxOO3sa1K3pFi5UAJQ10tSyhu0yn0AnRG13NWK6DlLKhLzZM5jhGJeeYYuwCL5fzJojflouHgebOO62gqNANkUcf7cWUBJRWjSAYuXe/C6rJOriZuUkC87QpffpYd2WaJmqnjAaj7NaqOTzk5ltpS39EjMenyXWWw1MPs7eEB/A/Rfol0cHzGqoXaIZAJVaEpW7ePWEj193CqSc6uh1nwAT15rvh63z2l1iPL0CbuF4GwZWsIZ6roirmwPpKY79kAls69EKsa7bydSQuYpbU5otkT20FIbtHmyFMYpJzYM6sQHoljO2AHWmWChkYtqglbFPrQgwIrsAHbJtmzNcmbXLUm1AY+SjZd1UYqPBjFDb7w== your_email@example.com",
-		Enabled: false,
+	getInput := &GetSnapshotInput{
+		Name: createdSnapshot.Name,
 	}
-	updateSSHKeyOutput, err := sshKeyClient.UpdateSSHKey(&updateSSHKeyInput)
+
+	snapshot, err := sClient.GetSnapshot(getInput)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if updateSSHKeyOutput.Enabled != updateSSHKeyInput.Enabled {
-		t.Fatalf("Key not successfully updated \nDesired: %s \nActual: %s", updateSSHKeyInput.Key, updateSSHKeyOutput.Key)
+	// Don't need to tear down the Snapshot, it's attached to the instance
+	log.Printf("Snapshot Retrieved: %+v", snapshot)
+	if !reflect.DeepEqual(snapshot.Name, createdSnapshot.Name) {
+		t.Fatal("Snapshot Name mismatch! Got: %s Expected: %s", snapshot.Name, createdSnapshot.Name)
 	}
-	log.Printf("Successfully updated ssh key\n")
 }
 
-func getSSHKeysClient() (*SSHKeysClient, error) {
+func tearDownSnapshots(t *testing.T, snapshotsClient *SnapshotsClient, name string) {
+	log.Printf("Deleting Snapshot %s", name)
+
+	deleteRequest := &DeleteSnapshotInput{
+		Name: name,
+	}
+	if err := snapshotsClient.DeleteSnapshot(deleteRequest); err != nil {
+		t.Fatalf("Error deleting snapshot, dangling resources may occur: %v", err)
+	}
+}
+
+func getSnapshotsTestClients() (*SnapshotsClient, *InstancesClient, error) {
 	client, err := getTestClient(&opc.Config{})
 	if err != nil {
-		return &SSHKeysClient{}, err
+		return nil, nil, err
 	}
-
-	return client.SSHKeys(), nil
-}
-
-func tearDownSSHKey(t *testing.T, client *SSHKeysClient, name string) {
-	deleteSSHKeyInput := DeleteSSHKeyInput{
-		Name: name,
-	}
-	err := client.DeleteSSHKey(&deleteSSHKeyInput)
-	if err != nil {
-		t.Fatal(err)
-	}
-	log.Printf("Successfully deleted SSH Key")
+	return client.Snapshots(), client.Instances(), nil
 }
