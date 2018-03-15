53824c23790b0d9a5ee9aaa14b58266adf60f193
= <mbfrahry@gmail.com>
2018-03-15 17:23:40 -0600

    Get/Update default Access Rules

--- database/access_rules_test.go
+++ database/access_rules_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/go-oracle-terraform/helper"
 	"github.com/hashicorp/go-oracle-terraform/opc"
 	"github.com/kylelemons/godebug/pretty"
+	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
 
 // Testing Parameters Used
@@ -111,6 +112,89 @@ func TestAccAccessRulesLifeCycle(t *testing.T) {
 	// Test Assertions
 	if diff := pretty.Compare(result, expected); diff != "" {
 		t.Fatalf("Diff creating AccessRule: (-got, +want):\n%s", diff)
+	}
+}
+
+func TestGetDefaultAccessRule_Basic(t *testing.T) {
+	sClient, aClient, err := getAccessRulesTestClients()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var instanceName string
+	if v := os.Getenv("OPC_TEST_DB_INSTANCE"); v == "" {
+		// First Create a Service Instance
+		sInstance, err := sClient.createTestServiceInstance()
+		if err != nil {
+			t.Fatalf("Error creating Service Instance: %s", err)
+		}
+		defer destroyServiceInstance(t, sClient, sInstance.Name)
+		instanceName = sInstance.Name
+	} else {
+		log.Print("Using already created DB Service Instance")
+		instanceName = v
+	}
+
+	getInput := &GetDefaultAccessRuleInput{
+		ServiceInstanceID: instanceName,
+	}
+
+	result, err := aClient.GetDefaultAccessRules(getInput)
+	if err != nil {
+		t.Fatalf("Error reading AccessRule: %s", err)
+	}
+
+	expected := &DefaultAccessRuleInfo{
+		EnableSSH:        utils.Bool(true),
+		EnableHTTP:       utils.Bool(false),
+		EnableHTTPSSL:    utils.Bool(false),
+		EnableDBConsole:  utils.Bool(false),
+		EnableDBExpress:  utils.Bool(false),
+		EnableDBListener: utils.Bool(false),
+	}
+
+	if diff := pretty.Compare(result, expected); diff != "" {
+		t.Fatalf("Diff on Default Access Rules: (-got, +want):\n%s", diff)
+	}
+}
+
+func TestUpdateDefaultAccessRule_Basic(t *testing.T) {
+	sClient, aClient, err := getAccessRulesTestClients()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var instanceName string
+	if v := os.Getenv("OPC_TEST_DB_INSTANCE"); v == "" {
+		// First Create a Service Instance
+		sInstance, err := sClient.createTestServiceInstance()
+		if err != nil {
+			t.Fatalf("Error creating Service Instance: %s", err)
+		}
+		defer destroyServiceInstance(t, sClient, sInstance.Name)
+		instanceName = sInstance.Name
+	} else {
+		log.Print("Using already created DB Service Instance")
+		instanceName = v
+	}
+
+	expected := &DefaultAccessRuleInfo{
+		ServiceInstanceID: instanceName,
+		EnableSSH:         utils.Bool(false),
+		EnableHTTP:        utils.Bool(true),
+		EnableHTTPSSL:     utils.Bool(true),
+		EnableDBConsole:   utils.Bool(false),
+		EnableDBExpress:   utils.Bool(true),
+		EnableDBListener:  utils.Bool(false),
+	}
+
+	result, err := aClient.UpdateDefaultAccessRules(expected)
+	if err != nil {
+		t.Fatalf("Error updating AccessRule: %s", err)
+	}
+
+	if diff := pretty.Compare(result, expected); diff != "" {
+		t.Fatalf("Diff on Default Access Rules: (-got, +want):\n%s", diff)
 	}
 }
 
